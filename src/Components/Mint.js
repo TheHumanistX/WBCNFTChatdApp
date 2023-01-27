47d7a89dd91f509321a93c819c7a7b5bfcfca72b
Brian H (Tirwander) <brianfhancock@gmail.com>
2023-01-27 14:04:35 -0500

    next day complete

---
+++ src/Components/Mint.js
@@ -0,0 +1,12 @@
+import "./Mint.css";
+
+function Mint(props) {
+
+    return (
+        <div>
+            <button class="mintButton"><a href="">Mint</a></button>
+        </div>
+    );
+}
+
+export default Mint;
