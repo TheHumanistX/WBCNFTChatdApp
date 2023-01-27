8309cbc70e14c6b84c28fe7d92bdde817ab9d008
Brian H (Tirwander) <brianfhancock@gmail.com>
2023-01-27 18:42:24 -0500

    End day 4, also messing with adding the date and time of each chat message...

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,18 +1,67 @@
-import { useState } from "react";
+import { EtherscanProvider } from "@ethersproject/providers";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 import Chat from './Chat.js';
 import "./Homepage.css";
+import Mint from "./Mint.js";
+import MintABI from './MintABI.json';
 
 function Homepage(props) {
     const [message, setMessage] = useState("");
+    const [allChats, setAllChats] = useState([]);
+
+    const sendMessage = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract("0x35F7f83F7d153e9c4A2E9B07e1302D46E259b5AD", MintABI, signer);
+
+        const tryToSend = await contract.addMessage(message, 0);
+        console.log(message);
+    }
+
+    const getMessages = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract("0x35F7f83F7d153e9c4A2E9B07e1302D46E259b5AD", MintABI, signer);
+
+        const totalMessages = await contract.totalMessages();
+
+        setAllChats([]);
+
+        for (var i = 0; i < totalMessages; i++) {
+            const currentMessage = await contract.Messages(i);
+            const timeStamp = currentMessage.timestamp.toNumber();
+            const date = new Date(timeStamp);
+            const currentMessageArray = [...currentMessage];
+            currentMessageArray[4] = currentMessageArray[4].toNumber();
+            currentMessageArray[4] = new Date(currentMessageArray[4]);
+            //console.log(currentMessage);
+            //console.log(timeStamp);
+            //console.log(currentMessage.timestamp);
+            setAllChats(prevChat => [...prevChat, currentMessageArray]);
+            console.log(currentMessageArray);
+            console.log(allChats)
+        }
+
+    }
+
+    useEffect(() => {
+        getMessages();
+    }, []);
     return (
         <div>
 
             <section>
                 <div className="hero">
                     <div className="chatMessage">
-                        <Chat text="YO" image="https://yt3.ggpht.com/ytc/AMLnZu-2DrkobCQd6ri63wO9SuMFGyTbyMhD5kQ6Up2N=s900-c-k-c0x00ffffff-no-rj" data="Oct 20, 2022 8:12PM" />
-                        <Chat text="Chat message" image="https://yt3.ggpht.com/ytc/AMLnZu-2DrkobCQd6ri63wO9SuMFGyTbyMhD5kQ6Up2N=s900-c-k-c0x00ffffff-no-rj" data="Oct 20, 2022 8:12PM" />
-                        <Chat text="This is a test" image="https://yt3.ggpht.com/ytc/AMLnZu-2DrkobCQd6ri63wO9SuMFGyTbyMhD5kQ6Up2N=s900-c-k-c0x00ffffff-no-rj" data="Oct 20, 2022 8:12PM" />
+                        {allChats.map((item =>
+
+
+
+                            <Chat key={item} text={item.messageText} image="https://yt3.ggpht.com/ytc/AMLnZu-2DrkobCQd6ri63wO9SuMFGyTbyMhD5kQ6Up2N=s900-c-k-c0x00ffffff-no-rj" data={item.sender} />
+
+                        ))}
+
                     </div>
 
                     <div className="Send">
@@ -22,11 +71,11 @@ function Homepage(props) {
                             value={message}
                             onChange={(e) => setMessage(e.target.value)}
                         />
-                        <button class="header-cta"><a href="">Send</a></button>
+                        <button className="header-cta"><a onClick={sendMessage} href="#" > Send</a></button>
                     </div>
                 </div>
-            </section>
-        </div>
+            </section >
+        </div >
     );
 }
 
