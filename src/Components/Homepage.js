defc1cc04a6419df92eeaa1c704d6f5901ebde4e
TheHumanistX <thehumanistx@gmail.com>
2023-01-29 17:30:02 -0500

    More work on making this dApp more streamlined and have less rerendering issues

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { EtherscanProvider } from "@ethersproject/providers";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
@@ -6,16 +7,21 @@ import "./Homepage.css";
 import Mint from "./Mint.js";
 import MintABI from './MintABI.json';
 import moment from 'moment';
+import MemoizedNFTList from './DropDownMenu.js';
 
 
 
 function Homepage(props) {
+    let memoizedNFTlist;
     const [message, setMessage] = useState("");
     const [allChats, setAllChats] = useState([]);
     const [pagination, setPagination] = useState(0);
     const [NFT, setNFT] = useState(0);
     const [NFTlist, setNFTlist] = useState([]);
     const [noMoreMessages, setNoMoreMessages] = useState(false);
+    const [contract, setContract] = useState(null);
+    const [provider, setProvider] = useState({});
+    const [signer, setSigner] = useState({});
 
     const timeConversion = (timeStampBigNumber) => {
         const timeStampInt = timeStampBigNumber.toNumber();
@@ -34,9 +40,12 @@ function Homepage(props) {
 
     const getMessages = async () => {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
+        console.log("Provider: " + provider);
         await provider.send("eth_requestAccounts");
         const signer = provider.getSigner();
+        console.log("Signer: " + signer);
         const contract = new ethers.Contract("0x35F7f83F7d153e9c4A2E9B07e1302D46E259b5AD", MintABI, signer);
+        console.log("Contract: " + contract);
 
         const totalMessages = await contract.totalMessages();
         setNoMoreMessages(Math.floor(totalMessages / ((pagination + 1) * 10)) <= 0 ? true : false)
@@ -51,7 +60,7 @@ function Homepage(props) {
             console.log("NFT Held Id: " + currentNFT.toNumber());
             setNFTlist(old => [...old, currentNFT]);
         }
-
+        
         setAllChats([]);
 
         for (var i = starting; i > starting - displayPerPage; i--) {
@@ -75,14 +84,15 @@ function Homepage(props) {
 
             <section>
                 <div className="hero">
-                    <button key="Back-Button" className="header-cta"><a onClick={() => noMoreMessages ? null : setPagination((old) => old + 1) } href="#" >Back</a></button>
-                    <button key="Forward-Button" className="header-cta"><a onClick={() => pagination > 0 ? setPagination((old) => old - 1) : null} href="#" >Forward</a></button>
+                    <button key="Back-Button" className="header-cta"><a onClick={() => noMoreMessages ? null : setPagination((old) => old + 1) } href="#" >Older</a></button>
+                    <button key="Forward-Button" className="header-cta"><a onClick={() => pagination > 0 ? setPagination((old) => old - 1) : null} href="#" >Newer
+                    </a></button>
                     <div className="chatMessage">
                         {allChats.map((item, index) => {
                             const date = item[4].toString();
 
                             return (
-                                <Chat key={index} text={item[3]} image="https://yt3.ggpht.com/ytc/AMLnZu-2DrkobCQd6ri63wO9SuMFGyTbyMhD5kQ6Up2N=s900-c-k-c0x00ffffff-no-rj" data={date} sender={item[1]} />
+                                <Chat key={item[0]} text={item[3]} image="https://yt3.ggpht.com/ytc/AMLnZu-2DrkobCQd6ri63wO9SuMFGyTbyMhD5kQ6Up2N=s900-c-k-c0x00ffffff-no-rj" data={date} sender={item[1]} />
 
                             )
                         })}
@@ -90,13 +100,14 @@ function Homepage(props) {
                     </div>
 
                     <div className="Send">
-                        <select onChange={(e) => setNFT(e.target.value)} name="NFTid" id="NFTid">
+                        {/* <select value={NFT} key="NFTDropDownSelection" onChange={(e) => setNFT(e.target.value)} name="NFTid" id="NFTid">
                                 <option key="ChooseNFT">Choose An NFT</option>
-                            {NFTlist.map((item, index) => (
+                            {memoizedNFTlist.map((item, index) => (
                                 <option key={index} value={item.toString()}>{item.toString()}</option>
                             ))}
 
-                        </select>
+                        </select> */}
+                        <MemoizedNFTList NFTlist = {NFTlist} setNFT = {setNFT} />
 
                         <input
                             className="textInput"
@@ -109,6 +120,7 @@ function Homepage(props) {
                 </div>
             </section >
         </div >
+        
     );
 }
 
